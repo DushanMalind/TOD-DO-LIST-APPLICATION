4a2affbc0d10b7f4655064d7849043bbdda9bf58
DushanMalind <dushanmalinda010@gmail.com>
2025-02-09 00:32:52 +0530

    Todo Done

--- src/main/java/lk/google/todolistapplication/entity/Todo.java
+++ src/main/java/lk/google/todolistapplication/entity/Todo.java
@@ -1,5 +1,13 @@
 package lk.google.todolistapplication.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+import java.util.UUID;
+
 /**
  * `@authority` DUSHAN MALINDA
  * 00:31
@@ -8,5 +16,23 @@ package lk.google.todolistapplication.entity;
  * To-Do-List-Application
  * lk.google.todolistapplication.entity
  */
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Todo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String title;
+    private String description;
+    private Boolean completed;
+    private String priority;
+    private Date dueDate;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
 }
