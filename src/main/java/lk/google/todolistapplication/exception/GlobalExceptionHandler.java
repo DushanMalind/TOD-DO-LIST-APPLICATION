abd4f8c4b6ec3d7060bdd87aece0594fb6b5ace7
DushanMalind <dushanmalinda010@gmail.com>
2025-02-09 00:50:28 +0530

    GlobalExceptionHandler done

--- src/main/java/lk/google/todolistapplication/exception/GlobalExceptionHandler.java
+++ src/main/java/lk/google/todolistapplication/exception/GlobalExceptionHandler.java
@@ -1,5 +1,11 @@
 package lk.google.todolistapplication.exception;
 
+import lk.google.todolistapplication.dto.AuthResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
 /**
  * `@authority` DUSHAN MALINDA
  * 00:28
@@ -8,5 +14,15 @@ package lk.google.todolistapplication.exception;
  * To-Do-List-Application
  * lk.google.todolistapplication.exception
  */
+@ControllerAdvice
 public class GlobalExceptionHandler {
+    @ExceptionHandler({UnauthorizedException.class})
+    public ResponseEntity<AuthResponse> handleUnauthorizedException(UnauthorizedException ex) {
+        AuthResponse response = new AuthResponse(null, "Unauthorized access: " + ex.getMessage());
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(response);
+    }
+    @ExceptionHandler({RuntimeException.class})
+    public ResponseEntity<String> handleRuntimeException(RuntimeException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Error: " + ex.getMessage());
+    }
 }
