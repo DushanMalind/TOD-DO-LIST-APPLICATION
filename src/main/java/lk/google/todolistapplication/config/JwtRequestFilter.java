22d8f1c844cf0b0fc8608d38e3922d01189765fa
DushanMalind <dushanmalinda010@gmail.com>
2025-02-09 00:51:32 +0530

    JwtRequestFilter done

--- src/main/java/lk/google/todolistapplication/config/JwtRequestFilter.java
+++ src/main/java/lk/google/todolistapplication/config/JwtRequestFilter.java
@@ -1,5 +1,21 @@
 package lk.google.todolistapplication.config;
 
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lk.google.todolistapplication.service.impl.UserServiceImpl;
+import lk.google.todolistapplication.util.JwtUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
 /**
  * `@authority` DUSHAN MALINDA
  * 00:27
@@ -8,5 +24,37 @@ package lk.google.todolistapplication.config;
  * To-Do-List-Application
  * lk.google.todolistapplication.config
  */
-public class JwtRequestFilter {
+@Component
+public class JwtRequestFilter extends OncePerRequestFilter {
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @Autowired
+    private UserServiceImpl userService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String authorizationHeader = request.getHeader("Authorization");
+        String userName=null;
+        String jwt=null;
+
+        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer")){
+            jwt=authorizationHeader.substring(7);
+            userName=jwtUtil.getEmailFromToken(jwt);
+        }
+
+        if (userName != null && SecurityContextHolder.getContext().getAuthentication() == null){
+            UserDetails userDetails = this.userService.loadUserByUsername(userName);
+
+            if (jwtUtil.validateToken(jwt)){
+                UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken =
+                        new UsernamePasswordAuthenticationToken(userDetails,null,userDetails.getAuthorities());
+                usernamePasswordAuthenticationToken
+                        .setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+            }
+        }
+        filterChain.doFilter(request,response);
+    }
 }
+
